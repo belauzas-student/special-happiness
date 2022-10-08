23976b5339c995b1e39e2b055228d8da2bd2d094
belauzas-student <49696122+belauzas-student@users.noreply.github.com>
2022-10-09 00:14:21 +0300

    Nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const bodyDOM = document.querySelector('body');
+const headerDOM = bodyDOM.querySelector('header');
 
 let HTML = '';
 
@@ -7,5 +8,13 @@ for (let i=0; i< 5; ++i) {
 }
 
 bodyDOM.insertAdjacentElement("afterbegin", HTML);
+
+let linksHTML = '';
+for (let i=0; i<5; ++i) {
+    linksHTML += `<a href="#">Link</a>`;
+}
+
+const navHTML = `<nav>${linksHTML}</nav>`;
+headerDOM.innerHTML = navHTML;
 
 console.log(HTML);
