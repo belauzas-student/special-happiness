1fe509f9fe9b1a4dcde66d156f15eb67ce87eef9
belauzas-student <49696122+belauzas-student@users.noreply.github.com>
2022-10-09 00:00:31 +0300

    Few items

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const bodyDOM = document.querySelector('body');
+
+let HTML = '';
+
+for (let i=0; i< 5; ++i) {
+    HTML += `<div class="item">Item: ${i}</div>`;
+}
+
+bodyDOM.insertAdjacentElement("afterbegin", HTML);
